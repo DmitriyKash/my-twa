6e68613b564368ed5ab665971becfa1114e71261
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-04-18 23:44:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(), nodePolyfills()],
-//   base: '/',
-// });
-
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/my-twa/',
+  base: '/',
 });
+
+
+// export default defineConfig({
+//   plugins: [react(), nodePolyfills()],
+//   base: '/my-twa/',
+// });
