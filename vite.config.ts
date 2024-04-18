36d05b73f1c2a216dad71754c2b8d034d50dc348
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-04-18 23:52:01 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/',
+  base: '/my-twa/' // Указываем поддиректорию, соответствующую названию репозитория на GitHub
 });
 
-
+// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react(), nodePolyfills()],
-//   base: '/my-twa/',
+//   base: '/',
 // });
