18c08f6191fa0ea24bb7ba4529cb583b40b496fe
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-04-18 23:01:16 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// this manifest is used temporarily for development purposes
-const manifestUrl = 'https://DmitriyKash.github.io/my-twa/tonconnect-manifest.json';
-
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </TonConnectUIProvider>,
+  </React.StrictMode>,
 )
