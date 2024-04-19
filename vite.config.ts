0404103ae9a2d1ca615ada73514c08bcf10dea1a
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-04-19 16:39:07 +0300

    +

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,28 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills(),
-    VitePWA({})
-  ],
-  base: '/my-twa/', // Убедитесь, что это корректный публичный путь
-  build: {
-    rollupOptions: {
-      input: '/index.html', // Указываем исходный index.html
-      output: {
-        assetFileNames: `assets/[name]-[hash][extname]` // Формат имен файлов
-      }
-    }
-  }
-});
-
-
-
-// // https://vitejs.dev/config/
 // export default defineConfig({
-//   plugins: [react(), nodePolyfills()],
-//   base: '/',
+//   plugins: [
+//     react(),
+//     nodePolyfills(),
+//     VitePWA({})
+//   ],
+//   base: '/my-twa/', // Убедитесь, что это корректный публичный путь
+//   build: {
+//     rollupOptions: {
+//       input: '/index.html', // Указываем исходный index.html
+//       output: {
+//         assetFileNames: `assets/[name]-[hash][extname]` // Формат имен файлов
+//       }
+//     }
+//   }
 // });
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), nodePolyfills()],
+  base: '/',
+});
 
