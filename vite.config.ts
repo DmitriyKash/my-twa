fd16186c0e1650b5ce1eb3892a1800038fd46a4a
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-04-19 20:38:53 +0300

    +

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // export default defineConfig({
 //   plugins: [
