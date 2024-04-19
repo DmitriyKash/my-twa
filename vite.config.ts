266461edf8e416467a44d7200c2288cc192b5dc7
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-04-19 15:58:19 +0300

    +

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/my-twa/' // Указываем поддиректорию, соответствующую названию репозитория на GitHub
+  base: '/my-twa/',
+  build: {
+    rollupOptions: {
+      external: ['/my-twa/assets/index-Db9LfXqK.js'],
+      output: {
+        assetFileNames: `assets/[name]-[hash][extname]`
+      }
+    }
+  }
 });
+
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
